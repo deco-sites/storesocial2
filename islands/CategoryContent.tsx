41437fc2448b0d4106b147a27074c51760d1edcd
Jonas Moraes Di Pierro <j.dipierro@socialsa.com>
2025-11-05 17:00:19 -0300

    fixing versions of dependencies

--- islands/CategoryContent.tsx
+++ islands/CategoryContent.tsx
@@ -1,104 +1,84 @@
 import { Picture, Source } from "apps/website/components/Picture.tsx";
-import { SectionProps } from "deco/types.ts";
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 import { useSignal } from "@preact/signals";
 import Icon from "../components/ui/Icon.tsx";
-
-
-
+import { type SectionProps } from "@deco/deco";
 /**
  * @titleBy matcher
  */
 export interface Banner {
-  /** @description RegExp para habilitar este banner na URL atual. Use, por exemplo, /feminino/* para exibir este conteúdo na categoria feminina  */
-  matcher: string;
-  image: {
-    /** @description Imagem do banner para telas grandes */
-    desktop: ImageWidget;
-    /** @description Imagem do banner para telas pequenas */
-    mobile: ImageWidget;
-    /** @description Alt da Imagem */
-    alt?: string;
-  };
-  /**  
-  * @title Título conteúdo
-  * @description Título da sessão de conteúdo
-  */
-  titleContent?: string;
-  /**  
- * @title Cor do título conteúdo
- * @description use padrão HEX. Exepmplo: #00B4FF
- */
-  titleContentColor?: string;
-  /**  
- * @title Subtítulo conteúdo
- * @description Subtítulo da sessão de conteúdo
- */
-  subtitleContent?: string;
-  /**  
-  * @title Cor do subtítulo conteúdo
-  * @description use padrão HEX. Exepmplo: #00B4FF
-  */
-  subtitleContentColor?: string;
-  /** 
-   * @title Texto conteúdo
-   * @description texto da sessão de conteúdo
-  */
-  textContent?: string;
-  /**
-  * @title Ícone conteúdo
-  * @description Ícone da sessão de conteúdo
-  */
-  iconContent?: ImageWidget;
+    /** @description RegExp para habilitar este banner na URL atual. Use, por exemplo, /feminino/* para exibir este conteúdo na categoria feminina  */
+    matcher: string;
+    image: {
+        /** @description Imagem do banner para telas grandes */
+        desktop: ImageWidget;
+        /** @description Imagem do banner para telas pequenas */
+        mobile: ImageWidget;
+        /** @description Alt da Imagem */
+        alt?: string;
+    };
+    /**
+    * @title Título conteúdo
+    * @description Título da sessão de conteúdo
+    */
+    titleContent?: string;
+    /**
+   * @title Cor do título conteúdo
+   * @description use padrão HEX. Exepmplo: #00B4FF
+   */
+    titleContentColor?: string;
+    /**
+   * @title Subtítulo conteúdo
+   * @description Subtítulo da sessão de conteúdo
+   */
+    subtitleContent?: string;
+    /**
+    * @title Cor do subtítulo conteúdo
+    * @description use padrão HEX. Exepmplo: #00B4FF
+    */
+    subtitleContentColor?: string;
+    /**
+     * @title Texto conteúdo
+     * @description texto da sessão de conteúdo
+    */
+    textContent?: string;
+    /**
+    * @title Ícone conteúdo
+    * @description Ícone da sessão de conteúdo
+    */
+    iconContent?: ImageWidget;
 }
 const DEFAULT_PROPS = {
-  banners: [
-    {
-      image: {
-        mobile:
-          "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/91102b71-4832-486a-b683-5f7b06f649af",
-        desktop:
-          "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/ec597b6a-dcf1-48ca-a99d-95b3c6304f96",
-        alt: "a",
-      },
-      titleContent: "Woman",
-      titleContentColor: "#000",
-      subtitleContent: "NOT",
-      subtitleContentColor: "#00B4FF",
-      textContent: "As",
-      matcher: "/*",
-      iconContent: "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/91102b71-4832-486a-b683-5f7b06f649af"
-    },
-  ],
+    banners: [
+        {
+            image: {
+                mobile: "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/91102b71-4832-486a-b683-5f7b06f649af",
+                desktop: "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/ec597b6a-dcf1-48ca-a99d-95b3c6304f96",
+                alt: "a",
+            },
+            titleContent: "Woman",
+            titleContentColor: "#000",
+            subtitleContent: "NOT",
+            subtitleContentColor: "#00B4FF",
+            textContent: "As",
+            matcher: "/*",
+            iconContent: "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/91102b71-4832-486a-b683-5f7b06f649af"
+        },
+    ],
 };
-
 function Banner(props: SectionProps<ReturnType<typeof loader>>) {
-  const { banner } = props;
-  if (!banner) {
-    return null;
-  }
-  const textCategoryState = useSignal(false);
-
-  const { titleContent, titleContentColor, subtitleContent, subtitleContentColor, textContent, image, iconContent, title, matcher } = banner;
-
-
-  return (
-    <div class="flex flex-col md-tablet:mt-4">
+    const { banner } = props;
+    if (!banner) {
+        return null;
+    }
+    const textCategoryState = useSignal(false);
+    const { titleContent, titleContentColor, subtitleContent, subtitleContentColor, textContent, image, iconContent, title, matcher } = banner;
+    return (<div class="flex flex-col md-tablet:mt-4">
       <Picture preload class="relative flex justify-center">
-        <Source
-          src={image.mobile}
-          width={360}
-          height={120}
-          media="(max-width: 767px)"
-        />
-        <Source
-          src={image.desktop}
-          width={1440}
-          height={200}
-          media="(min-width: 767px)"
-        />
-        <img class="w-full" src={image.desktop} alt={titleContent} />
+        <Source src={image.mobile} width={360} height={120} media="(max-width: 767px)"/>
+        <Source src={image.desktop} width={1440} height={200} media="(min-width: 767px)"/>
+        <img class="w-full" src={image.desktop} alt={titleContent}/>
         <div class="brand-breadcrumb max-w-[1340px] mx-auto my-0 w-full absolute bottom-4 ml-5">
             <div class="flex items-center max-w-fit gap-[10px] px-[10px] py-[6px] bg-[#00000099] shadow-[0_4px_30px_0px_rgba(0,0,0,0.1)] backdrop-filter backdrop-blur-[0px] rounded-[5px]">
               <div>
@@ -107,7 +87,7 @@ function Banner(props: SectionProps<ReturnType<typeof loader>>) {
                 </a>
               </div>
               <div>
-                <Icon size={10} id="ChevronRightBreadcrumb" strokeWidth={2} /> 
+                <Icon size={10} id="ChevronRightBreadcrumb" strokeWidth={2}/> 
               </div>
               <div>
                 <a class="text-white text-base font-normal leading-[18px]" href={matcher}>
@@ -132,7 +112,7 @@ function Banner(props: SectionProps<ReturnType<typeof loader>>) {
             </h1>
           </div>
           <div>
-            <img class="max-w-20 w-20" src={iconContent} alt={titleContent} />
+            <img class="max-w-20 w-20" src={iconContent} alt={titleContent}/>
           </div>
         </div>
         {/* Content Desktop */}
@@ -147,7 +127,7 @@ function Banner(props: SectionProps<ReturnType<typeof loader>>) {
           </h1>
         </div>
         <div class="md-tablet:hidden ">
-          <img class="max-w-20 w-20" src={iconContent} alt={titleContent} />
+          <img class="max-w-20 w-20" src={iconContent} alt={titleContent}/>
         </div>
         <div class="max-w-[960px] flex flex-col">
           <h2>
@@ -160,22 +140,14 @@ function Banner(props: SectionProps<ReturnType<typeof loader>>) {
           </button>
         </div>
       </div>
-    </div>
-  );
+    </div>);
 }
-
 export interface Props {
-  banners?: Banner[];
+    banners?: Banner[];
 }
-
 export const loader = (props: Props, req: Request) => {
-  const { banners } = { ...DEFAULT_PROPS, ...props };
-
-  const banner = banners.find(({ matcher }) =>
-    new URLPattern({ pathname: matcher }).test(req.url)
-  );
-
-  return { banner };
+    const { banners } = { ...DEFAULT_PROPS, ...props };
+    const banner = banners.find(({ matcher }) => new URLPattern({ pathname: matcher }).test(req.url));
+    return { banner };
 };
-
 export default Banner;
--- sections/Product/ShelfWithImage.tsx
+++ sections/Product/ShelfWithImage.tsx
@@ -1,33 +1,23 @@
-import { Section } from "deco/blocks/section.ts";
 import Image from "apps/website/components/Image.tsx";
 import type { ImageWidget } from "apps/admin/widgets.ts";
-
+import { type Section } from "@deco/deco/blocks";
 export interface Props {
-  section: Section;
-  image: {
-    src: ImageWidget;
-    alt?: string;
-    href?: string;
-  };
+    section: Section;
+    image: {
+        src: ImageWidget;
+        alt?: string;
+        href?: string;
+    };
 }
-
 export default function ShelfWithImage({ section, image }: Props) {
-  return (
-    <div class="container">
+    return (<div class="container">
       <div class="grid md:grid-cols-2 grid-cols-1 bg-neutral-content">
         <a href={image.href} class="full-phone:hidden">
-          <Image
-            src={image.src}
-            class="w-full h-full object-cover"
-            width={720}
-            height={640}
-            alt={image.alt}
-          />
+          <Image src={image.src} class="w-full h-full object-cover" width={720} height={640} alt={image.alt}/>
         </a>
         <div class="max-w-[425px] mx-auto flex items-center overflow-x-hidden ShelfWithImage">
-          <section.Component {...section.props} />
+          <section.Component {...section.props}/>
         </div>
       </div>
-    </div>
-  );
+    </div>);
 }
